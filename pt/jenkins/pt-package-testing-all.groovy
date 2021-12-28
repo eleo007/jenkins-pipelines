1b8d619ce1a22cb219c24ac70027478b79731cb4
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2021-12-28 11:06:10 +0200

    Add repo name selection

--- pt/jenkins/pt-package-testing-all.groovy
+++ pt/jenkins/pt-package-testing-all.groovy
@@ -11,6 +11,7 @@ void runNodeBuild(String node_to_test) {
             string(name: 'install_repo', value: params.install_repo),
             string(name: 'node_to_test', value: node_to_test),
             string(name: 'git_repo', value: params.git_repo),
+            string(name: 'repo_name', value: params.repo_name),
             booleanParam(name: 'skip_ps57', value: params.skip_ps57),
             booleanParam(name: 'skip_ps80', value: params.skip_ps80),
             booleanParam(name: 'skip_pxc57', value: params.skip_pxc57),
