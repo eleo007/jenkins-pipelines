291523002623dbb47c43ba73ee1d2e53f9f9df6a
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2022-08-22 17:37:14 +0300

    Create local pdps files and add MAJOR_REPO boolean

---
+++ pdps/pdps-eleonora.groovy
@@ -0,0 +1,147 @@
+library changelog: false, identifier: "lib@DISTMYSQL-213-repo80", retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/eleo007/jenkins-pipelines.git'
+])
+
+
+pipeline {
+    agent {
+    label 'min-centos-7-x64'
+    }
+    environment {
+      PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin';
+      MOLECULE_DIR = "molecule/pdmysql/${SCENARIO}";
+    }
+    parameters {
+        choice(
+            name: 'PLATFORM',
+            description: 'For what platform (OS) need to test',
+            choices: pdmysqlOperatingSystems()
+        )
+        choice(
+            name: 'REPO',
+            description: 'Repo for testing',
+            choices: [
+                'testing',
+                'experimental',
+                'release'
+            ]
+        )
+        string(
+            defaultValue: '8.0.29',
+            description: 'Percona Server version for test',
+            name: 'VERSION'
+        )
+        string(
+            defaultValue: '2.3.2',
+            description: 'Proxysql version for test',
+            name: 'PROXYSQL_VERSION'
+        )
+        string(
+            defaultValue: '8.0.29',
+            description: 'PXB version for test',
+            name: 'PXB_VERSION'
+        )
+        string(
+            defaultValue: '3.4.0',
+            description: 'Percona toolkit version for test',
+            name: 'PT_VERSION'
+        )
+        string(
+            defaultValue: '3.2.6',
+            description: 'Percona Orchestrator version for test',
+            name: 'ORCHESTRATOR_VERSION'
+        )
+        choice(
+            name: 'SCENARIO',
+            description: 'Scenario for test',
+            choices: pdpsScenarios()
+        )
+        string(
+            defaultValue: 'master',
+            description: 'Branch for package-testing repository',
+            name: 'TESTING_BRANCH'
+        )
+        string(
+            defaultValue: 'master',
+            description: 'Tests will be run from branch of  https://github.com/percona/orchestrator',
+            name: 'ORCHESTRATOR_TESTS_VERSION'
+        )
+        choice(
+            name: 'DESTROY_ENV',
+            description: 'Destroy VM after tests',
+            choices: [
+                'yes',
+                'no'
+            ]
+        )
+        booleanParam(
+            name: 'MAJOR_REPO', 
+            description: "Enable to use pdps-8.0 repo instead of pdps-8.0.XX"
+        )
+    }
+    options {
+          withCredentials(moleculePdpsJenkinsCreds())
+          disableConcurrentBuilds()
+    }
+    stages {
+        stage('Set build name'){
+            steps {
+                script {
+                    currentBuild.displayName = "${env.BUILD_NUMBER}-${env.PLATFORM}-${env.SCENARIO}"
+                }
+            }
+        }
+        stage('Checkout') {
+            steps {
+                deleteDir()
+                git poll: false, branch: TESTING_BRANCH, url: 'https://github.com/Percona-QA/package-testing.git'
+            }
+        }
+        stage ('Prepare') {
+            steps {
+                script {
+                    installMolecule()
+                }
+            }
+        }
+        stage ('Create virtual machines') {
+            steps {
+                script{
+                    moleculeExecuteActionWithScenario(env.MOLECULE_DIR, "create", env.PLATFORM)
+                }
+            }
+        }
+        stage ('Run playbook for test') {
+            steps {
+                script{
+                    moleculeExecuteActionWithScenario(env.MOLECULE_DIR, "converge", env.PLATFORM)
+                }
+            }
+        }
+        stage ('Start testinfra tests') {
+            steps {
+                script{
+                    moleculeExecuteActionWithScenario(env.MOLECULE_DIR, "verify", env.PLATFORM)
+                }
+            }
+        }
+        stage ('Start Cleanup ') {
+            steps {
+                script {
+                    moleculeExecuteActionWithScenario(env.MOLECULE_DIR, "cleanup", env.PLATFORM)
+                }
+            }
+        }
+    }
+    post {
+        always {
+            script {
+                if (env.DESTROY_ENV == "yes") {
+                    moleculeExecuteActionWithScenario(env.MOLECULE_DIR, "destroy", env.PLATFORM)
+                    junit "${MOLECULE_DIR}/report.xml"
+                }
+            }
+        }
+    }
+}
