67bf6bbdfee9032b7f456766c1fa38ee5664b015
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2022-08-23 16:15:10 +0300

    create eleonora files and add MAJOR_REPO for pdps-parallel

---
+++ pdps/pdps-parallel-eleonora.groovy
@@ -0,0 +1,108 @@
+library changelog: false, identifier: "lib@DISTMYSQL-213-repo80", retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/eleo007/jenkins-pipelines.git'
+])
+
+pipeline {
+  agent {
+      label 'min-centos-7-x64'
+  }
+  environment {
+      PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin';
+      MOLECULE_DIR = "molecule/pdmysql/${SCENARIO}";
+
+  }
+  parameters {
+        choice(
+            name: 'REPO',
+            description: 'Repo for testing',
+            choices: [
+                'testing',
+                'experimental',
+                'release'
+            ]
+        )
+        string(
+            defaultValue: '8.0.29',
+            description: 'PDMYSQL version for test',
+            name: 'VERSION'
+         )
+        string(
+            defaultValue: '2.3.2',
+            description: 'Proxysql version for test',
+            name: 'PROXYSQL_VERSION'
+         )
+        string(
+            defaultValue: '8.0.29',
+            description: 'PXB version for test',
+            name: 'PXB_VERSION'
+         )
+        string(
+            defaultValue: '3.4.0',
+            description: 'Percona toolkit version for test',
+            name: 'PT_VERSION'
+         )
+        string(
+            defaultValue: '3.2.6',
+            description: 'Percona orchestrator version for test',
+            name: 'ORCHESTRATOR_VERSION'
+         )
+        choice(
+            name: 'SCENARIO',
+            description: 'PDMYSQL scenario for test',
+            choices: pdpsScenarios()
+        )
+        string(
+            defaultValue: 'master',
+            description: 'Branch for package-testing repository',
+            name: 'TESTING_BRANCH')
+        booleanParam(
+            name: 'MAJOR_REPO', 
+            description: "Enable to use major (pdps-8.0) repo instead of pdps-8.0.XX"
+        )
+        string(
+            defaultValue: 'master',
+            description: 'Tests will be run from branch of  https://github.com/percona/orchestrator',
+            name: 'ORCHESTRATOR_TESTS_VERSION')
+  }
+  options {
+          withCredentials(moleculePdpsJenkinsCreds())
+          disableConcurrentBuilds()
+  }
+    stages {
+        stage('Set build name'){
+          steps {
+                    script {
+                        currentBuild.displayName = "${env.BUILD_NUMBER}-${env.SCENARIO}"
+                    }
+                }
+            }
+        stage('Checkout') {
+            steps {
+                deleteDir()
+                git poll: false, branch: TESTING_BRANCH, url: 'https://github.com/eleo007/package-testing.git'
+            }
+        }
+        stage ('Prepare') {
+          steps {
+                script {
+                   installMolecule()
+             }
+           }
+        }
+        stage('Test') {
+          steps {
+                script {
+                    moleculeParallelTest(pdmysqlOperatingSystems(), env.MOLECULE_DIR)
+                }
+            }
+         }
+  }
+    post {
+        always {
+          script {
+              moleculeParallelPostDestroy(pdmysqlOperatingSystems(), env.MOLECULE_DIR)
+         }
+      }
+   }
+}
