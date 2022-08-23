7244119306d6ebef80131bfbcea235b27aa0cc31
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2022-08-23 17:11:43 +0300

    Update pdps-parallel

--- pdps/pdps-parallel-eleonora.groovy
+++ pdps/pdps-parallel-eleonora.groovy
@@ -53,17 +53,19 @@ pipeline {
             choices: pdpsScenarios()
         )
         string(
-            defaultValue: 'master',
+            defaultValue: 'DISTMYSQL-213-repo80',
             description: 'Branch for package-testing repository',
-            name: 'TESTING_BRANCH')
-        booleanParam(
-            name: 'MAJOR_REPO', 
-            description: "Enable to use major (pdps-8.0) repo instead of pdps-8.0.XX"
+            name: 'TESTING_BRANCH'
         )
         string(
             defaultValue: 'master',
             description: 'Tests will be run from branch of  https://github.com/percona/orchestrator',
-            name: 'ORCHESTRATOR_TESTS_VERSION')
+            name: 'ORCHESTRATOR_TESTS_VERSION'
+        )
+        booleanParam(
+            name: 'MAJOR_REPO',
+            description: "Enable to use major (pdps-8.0) repo instead of pdps-8.0.XX"
+        )
   }
   options {
           withCredentials(moleculePdpsJenkinsCreds())
