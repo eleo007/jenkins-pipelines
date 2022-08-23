1e1c13686cf4c1375cd1143a23ccd97066ce0edb
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2022-08-23 18:41:55 +0300

    add my files for pxc

---
+++ pdpxc/pdpxc-parallel-eleonora.groovy
@@ -0,0 +1,101 @@
+library changelog: false, identifier: "lib@DISTMYSQL-213-repo80", retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/eleo007/jenkins-pipelines.git'
+])
+
+
+pipeline {
+  agent {
+      label 'min-centos-7-x64'
+  }
+  environment {
+      PATH = '/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin';
+      MOLECULE_DIR = "molecule/pdmysql/${SCENARIO}";
+
+  }
+  parameters {
+        choice(
+            name: 'REPO',
+            description: 'Repo for testing',
+            choices: [
+                'testing',
+                'experimental',
+                'release'
+            ]
+        )
+        string(
+            defaultValue: '8.0.28',
+            description: 'PXC version for test',
+            name: 'VERSION'
+         )
+        string(
+            defaultValue: '2.0.18',
+            description: 'Proxysql version for test',
+            name: 'PROXYSQL_VERSION'
+         )
+        string(
+            defaultValue: '2.3.10',
+            description: 'HAProxy version for test',
+            name: 'HAPROXY_VERSION'
+         )
+        string(
+            defaultValue: '8.0.23',
+            description: 'PXB version for test',
+            name: 'PXB_VERSION'
+         )
+        string(
+            defaultValue: '3.3.1',
+            description: 'Percona toolkit version for test',
+            name: 'PT_VERSION'
+         )
+        choice(
+            name: 'SCENARIO',
+            description: 'Scenario for test',
+            choices: pdpxcScenarios()
+        )
+        string(
+            defaultValue: 'master',
+            description: 'Branch for testing repository',
+            name: 'TESTING_BRANCH')
+  }
+  options {
+          withCredentials(moleculePdpxcJenkinsCreds())
+          disableConcurrentBuilds()
+  }
+    stages {
+        stage('Set build name'){
+          steps {
+                    script {
+                        currentBuild.displayName = "${env.BUILD_NUMBER}-${env.SCENARIO}"
+                    }
+                }
+            }
+        stage('Checkout') {
+            steps {
+                deleteDir()
+                git poll: false, branch: TESTING_BRANCH, url: 'https://github.com/Percona-QA/package-testing.git'
+            }
+        }
+        stage ('Prepare') {
+          steps {
+                script {
+                   installMolecule()
+             }
+           }
+        }
+        stage('Test') {
+          steps {
+                script {
+                    moleculeParallelTest(pdpxcOperatingSystems(), env.MOLECULE_DIR)
+                }
+            }
+         }
+  }
+    post {
+        always {
+          script {
+              moleculeParallelPostDestroy(pdpxcOperatingSystems(), env.MOLECULE_DIR)
+         }
+      }
+   }
+}
