0de00638465fbf2a60d5b3f804512367bb446621
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2023-03-30 12:52:25 +0300

    Add orch docker test

---
+++ pdps/orchestrator_docker.groovy
@@ -0,0 +1,69 @@
+library changelog: false, identifier: "lib@DISTMYSQL-278_orch_docker_new", retriever: modernSCM([
+    $class: 'GitSCMSource',
+    remote: 'https://github.com/eleo007/jenkins-pipelines.git'
+])
+
+
+pipeline {
+  agent {
+      label "docker-32gb"
+  }
+
+  parameters {
+    choice(name: 'DOCKER_ACC', choices: ['percona','perconalab'], description: 'Docker repo to use: percona or perconalab')
+    string(name: 'PS_VERSION', defaultValue: '8.0.26-16', description: 'Full PS version')
+    string(name: 'PS_REVISION', defaultValue: '3d64165', description: 'Short git hash for release')
+  }
+
+  parameters {
+    choice(
+      name: 'DOCKER_ACC',
+      description: 'Docker repo to use: percona or perconalab',
+      choices: [
+        'percona',
+        'perconalab'
+      ]
+    )
+    string(
+      defaultValue: '3.2.6-8',
+      description: 'Full orchestrator version',
+      name: 'OCHESTARTOR_VERSION'
+    )
+    string(
+      defaultValue: '8.0.32-24',
+      description: 'Full PS version to test with orchestrator',
+      name: 'PS_VERSION'
+    )
+    string(
+      defaultValue: 'master',
+      description: 'Branch for package-testing repository',
+      name: 'TESTING_BRANCH'
+    )
+  }
+
+  stages {
+    stage('Run test') {
+      steps {
+          script {
+            currentBuild.displayName = "#${BUILD_NUMBER}-${PS_VERSION}-${OCHESTARTOR_VERSION}"
+            currentBuild.description = "${DOCKER_ACC}"
+          }
+          sh '''
+            # run test
+            export PATH=${PATH}:~/.local/bin
+            sudo yum install -y python3 python3-pip
+            rm -rf package-testing
+            git clone https://github.com/Percona-QA/package-testing.git -b ${TESTING_BRANCH} --depth 1
+            cd package-testing/docker-image-tests/orchestrator
+            pip3 install --user -r requirements.txt
+            ./run.sh
+          '''
+      } //end steps
+    } //end Run test stage
+  } //end stages
+  post {
+    always {
+      junit 'package-testing/docker-image-tests/orchestrator/report.xml'
+    }
+  }
+}
