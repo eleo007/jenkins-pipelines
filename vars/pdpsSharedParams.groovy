2263ef364b867c075bc1df9728a6447dad9cd9c5
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2023-08-03 16:42:21 +0300

    Check shared lib

---
+++ vars/pdpsSharedParams.groovy
@@ -0,0 +1,80 @@
+def call() {
+    return [
+        choice(
+            name: 'PLATFORM',
+            description: 'For what platform (OS) need to test',
+            choices: pdpsOperatingSystems()
+        )
+        choice(
+            name: 'REPO',
+            description: 'Repo for testing',
+            choices: [
+                'testing',
+                'experimental',
+                'release'
+            ]
+        )
+        string(
+            defaultValue: '8.0.33-25',
+            description: 'Percona Server version for test. Possible values are with and without percona release and build: 8.0.32, 8.0.32-24 OR 8.0.32-24.2',
+            name: 'VERSION'
+        )
+        string(
+            defaultValue: '2.5.1',
+            description: 'Proxysql version for test',
+            name: 'PROXYSQL_VERSION'
+        )
+        string(
+            defaultValue: '8.0.33-27',
+            description: 'PXB version for test. Possible values are with and without percona release and build: 8.0.32, 8.0.32-25 OR 8.0.32-25.1',
+            name: 'PXB_VERSION'
+        )
+        string(
+            defaultValue: '3.5.3',
+            description: 'Percona toolkit version for test',
+            name: 'PT_VERSION'
+        )
+        string(
+            defaultValue: '3.2.6-9',
+            description: 'Percona Orchestrator version for test',
+            name: 'ORCHESTRATOR_VERSION'
+        )
+        string(
+            defaultValue: '',
+            description: 'Orchestrator revision for version from https://github.com/percona/orchestrator . Empty by default (not checked).',
+            name: 'ORCHESTRATOR_REVISION'
+        )
+        choice(
+            name: 'SCENARIO',
+            description: 'Scenario for test',
+            choices: pdpsScenarios()
+        )
+        string(
+            defaultValue: 'master',
+            description: 'Branch for package-testing repository',
+            name: 'TESTING_BRANCH'
+        )
+        string(
+            defaultValue: 'Percona-QA',
+            description: 'Git account for package-testing repository',
+            name: 'TESTING_GIT_ACCOUNT'
+        )
+        string(
+            defaultValue: 'master',
+            description: 'Tests will be run from branch of  https://github.com/percona/orchestrator',
+            name: 'ORCHESTRATOR_TESTS_VERSION'
+        )
+        booleanParam(
+            name: 'MAJOR_REPO',
+            description: "Enable to use major (pdps-8.0) repo instead of pdps-8.0.XX"
+        )
+        choice(
+            name: 'DESTROY_ENV',
+            description: 'Destroy VM after tests',
+            choices: [
+                'yes',
+                'no'
+            ]
+        )
+    ]
+}
