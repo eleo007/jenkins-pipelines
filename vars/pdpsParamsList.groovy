23fb35efa2d1cbd794516272412755e93bc1e801
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2023-08-21 16:29:30 +0300

    Use vars file

--- vars/pdpsParamsList.groovy
+++ vars/pdpsParamsList.groovy
@@ -1,16 +1,3 @@
 def call() {
-        choice(
-            name: 'PLATFORM',
-            description: 'For what platform (OS) need to test',
-            choices: pdpsOperatingSystems()
-        )
-        choice(
-            name: 'REPO',
-            description: 'Repo for testing',
-            choices: [
-                'testing',
-                'experimental',
-                'release'
-            ]
-        )
+    return text(name: 'DEPLOY_TEXT', defaultValue: 'One\nTwo\nThree\n', description: '')
 }
