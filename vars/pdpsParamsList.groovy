563534debd945aceb0fef352620ed0e328cf2ad8
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2023-08-22 15:51:34 +0300

    try pdps values

--- vars/pdpsParamsList.groovy
+++ vars/pdpsParamsList.groovy
@@ -8,62 +8,7 @@ def call() {
                         'testing',
                         'experimental',
                         'release'
-                ]
-                )
-                string(
-                defaultValue: '8.0.33-25',
-                description: 'Percona Server version for test. Possible values are with and without percona release and build: 8.0.32, 8.0.32-24 OR 8.0.32-24.2',
-                name: 'VERSION'
-                )
-                string(
-                defaultValue: '',
-                description: 'Percona Server revision for test. Empty by default (not checked).',
-                name: 'PS_REVISION'
-                )
-                string(
-                defaultValue: '2.5.1',
-                description: 'Proxysql version for test',
-                name: 'PROXYSQL_VERSION'
-                )
-                string(
-                defaultValue: '8.0.33-27',
-                description: 'PXB version for test. Possible values are with and without percona release and build: 8.0.32, 8.0.32-25 OR 8.0.32-25.1',
-                name: 'PXB_VERSION'
-                )
-                string(
-                defaultValue: '3.5.3',
-                description: 'Percona toolkit version for test',
-                name: 'PT_VERSION'
-                )
-                string(
-                defaultValue: '3.2.6-9',
-                description: 'Percona orchestrator version for test',
-                name: 'ORCHESTRATOR_VERSION'
-                )
-                string(
-                defaultValue: '',
-                description: 'Orchestrator revision for version from https://github.com/percona/orchestrator . Empty by default (not checked).',
-                name: 'ORCHESTRATOR_REVISION'
-                )
-                choice(
-                name: 'SCENARIO',
-                description: 'PDMYSQL scenario for test',
-                choices: pdpsScenarios()
-                )
-                string(
-                defaultValue: 'master',
-                description: 'Branch for package-testing repository',
-                name: 'TESTING_BRANCH'
-                )
-                string(
-                defaultValue: 'Percona-QA',
-                description: 'Git account for package-testing repository',
-                name: 'TESTING_GIT_ACCOUNT'
-                )
-                string(
-                defaultValue: 'master',
-                description: 'Tests will be run from branch of  https://github.com/percona/orchestrator',
-                name: 'ORCHESTRATOR_TESTS_VERSION'
+                        ]
                 )
                 booleanParam(
                 name: 'MAJOR_REPO',
